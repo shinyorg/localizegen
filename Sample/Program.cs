5eab3308ada1b4961339fb65c4438fca935b9a3a
Allan Ritchie <allan.ritchie@gmail.com>
2023-09-10 11:25:04 -0400

    Cleanup

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -1,6 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.Runtime.CompilerServices;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sample;
+
+//var services = new ServiceCollection();
+//services.AddLocalization();
+//services.AddStronglyTypedLocalizations();
+//services.AddSingleton<SampleClass>();
 
 Console.WriteLine("Hello, World!");
-
-
--- Sample/SampleClass.cs
+++ Sample/SampleClass.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Sample;
 
-namespace Sample
+public class SampleClass
 {
-	internal class SampleClass
-	{
-	}
+	//public SampleClass(SampleClassLocalized localized)
+	//{
+
+	//}
 }
