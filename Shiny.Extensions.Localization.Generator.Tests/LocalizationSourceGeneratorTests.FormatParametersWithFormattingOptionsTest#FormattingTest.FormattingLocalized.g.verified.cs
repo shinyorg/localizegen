aef8b8b4ff8639b3ed2397f6ef66ddaeb84efa16
Allan Ritchie <allan.ritchie@gmail.com>
2025-09-09 09:05:51 -0400

    Fix tests

--- Shiny.Extensions.Localization.Generator.Tests/LocalizationSourceGeneratorTests.FormatParametersWithFormattingOptionsTest#FormattingTest.FormattingLocalized.g.verified.cs
+++ Shiny.Extensions.Localization.Generator.Tests/LocalizationSourceGeneratorTests.FormatParametersWithFormattingOptionsTest#FormattingTest.FormattingLocalized.g.verified.cs
@@ -15,49 +15,81 @@ public partial class FormattingLocalized
 	/// <summary>
 	/// Today is {0:MMM dd yyyy}
 	/// </summary>
-	public string DateFormatMessage => this.localizer["DateFormatMessage"];
+	public string DateFormatMessageFormat(object parameter0)
+	{
+		return string.Format(this.localizer["DateFormatMessage"], parameter0);
+	}
+
 	/// <summary>
 	/// Total cost: {0:C2}
 	/// </summary>
-	public string CurrencyFormatMessage => this.localizer["CurrencyFormatMessage"];
+	public string CurrencyFormatMessageFormat(object parameter0)
+	{
+		return string.Format(this.localizer["CurrencyFormatMessage"], parameter0);
+	}
+
 	/// <summary>
 	/// Success rate: {0:P2}
 	/// </summary>
-	public string PercentageFormatMessage => this.localizer["PercentageFormatMessage"];
+	public string PercentageFormatMessageFormat(object parameter0)
+	{
+		return string.Format(this.localizer["PercentageFormatMessage"], parameter0);
+	}
+
 	/// <summary>
 	/// Value: {0:N2}
 	/// </summary>
-	public string NumberFormatMessage => this.localizer["NumberFormatMessage"];
+	public string NumberFormatMessageFormat(object parameter0)
+	{
+		return string.Format(this.localizer["NumberFormatMessage"], parameter0);
+	}
+
 	/// <summary>
 	/// Event scheduled for {0:yyyy-MM-dd HH:mm:ss}
 	/// </summary>
-	public string CustomDateTimeFormat => this.localizer["CustomDateTimeFormat"];
+	public string CustomDateTimeFormatFormat(object parameter0)
+	{
+		return string.Format(this.localizer["CustomDateTimeFormat"], parameter0);
+	}
+
 	/// <summary>
 	/// Date: {0:MMM dd yyyy}, Amount: {1:C2}, Rate: {2:P1}
 	/// </summary>
-	public string MixedFormattingMessage => this.localizer["MixedFormattingMessage"];
+	public string MixedFormattingMessageFormat(object parameter0, object parameter1, object parameter2)
+	{
+		return string.Format(this.localizer["MixedFormattingMessage"], parameter0, parameter1, parameter2);
+	}
+
 	/// <summary>
 	/// From {0:yyyy-MM-dd} to {1:yyyy-MM-dd}
 	/// </summary>
-	public string DateRangeMessage => this.localizer["DateRangeMessage"];
+	public string DateRangeMessageFormat(object parameter0, object parameter1)
+	{
+		return string.Format(this.localizer["DateRangeMessage"], parameter0, parameter1);
+	}
+
 	/// <summary>
 	/// Report generated on {0:dddd, MMMM dd, yyyy} at {1:HH:mm:ss} for user {2}
 	/// </summary>
-	public string DetailedReportMessageFormat(object parameter2)
+	public string DetailedReportMessageFormat(object parameter0, object parameter1, object parameter2)
 	{
-		return string.Format(this.localizer["DetailedReportMessage"], parameter2);
+		return string.Format(this.localizer["DetailedReportMessage"], parameter0, parameter1, parameter2);
 	}
 
 	/// <summary>
 	/// Start: {0:HH:mm}, End: {0:HH:mm:ss}
 	/// </summary>
-	public string RepeatedFormattedParameter => this.localizer["RepeatedFormattedParameter"];
+	public string RepeatedFormattedParameterFormat(object parameter0)
+	{
+		return string.Format(this.localizer["RepeatedFormattedParameter"], parameter0);
+	}
+
 	/// <summary>
 	/// User {0} logged in at {1:yyyy-MM-dd HH:mm} from location {2}
 	/// </summary>
-	public string MixedParameterTypesFormat(object parameter0, object parameter2)
+	public string MixedParameterTypesFormat(object parameter0, object parameter1, object parameter2)
 	{
-		return string.Format(this.localizer["MixedParameterTypes"], parameter0, parameter2);
+		return string.Format(this.localizer["MixedParameterTypes"], parameter0, parameter1, parameter2);
 	}
 
 }
